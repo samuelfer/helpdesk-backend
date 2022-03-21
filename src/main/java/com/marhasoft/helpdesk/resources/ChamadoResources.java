8fa652c81a6ef031548e1b242d3d668e8a690be0
samuelfer <samuelfesant@gmail.com>
2022-03-20 22:17:27 -0300

    Add método para listar todos os chamados

--- src/main/java/com/marhasoft/helpdesk/resources/ChamadoResources.java
+++ src/main/java/com/marhasoft/helpdesk/resources/ChamadoResources.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping(value = "/chamados")
 public class ChamadoResources {
@@ -21,5 +24,12 @@ public class ChamadoResources {
     public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id) {
         Chamado chamado = chamadoService.findById(id);
         return ResponseEntity.ok().body(new ChamadoDTO(chamado));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ChamadoDTO>> findAll() {
+        List<Chamado> chamados = chamadoService.findAll();
+        List<ChamadoDTO> chamadosDTO = chamados.stream().map(obj -> new ChamadoDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(chamadosDTO);
     }
 }
--- src/main/java/com/marhasoft/helpdesk/services/ChamadoService.java
+++ src/main/java/com/marhasoft/helpdesk/services/ChamadoService.java
@@ -7,6 +7,7 @@ import com.marhasoft.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,5 +21,9 @@ public class ChamadoService {
     public Chamado findById(Integer id) {
         Optional<Chamado> chamado = chamadoRepository.findById(id);
         return chamado.orElseThrow(() -> new ObjectNotFoundException("Chamado não encontado com id: "+id));
+    }
+
+    public List<Chamado> findAll() {
+        return chamadoRepository.findAll();
     }
 }
