d19870c98caa008034c23fb44d7091ec6b0922c3
samuelfer <samuelfesant@gmail.com>
2022-03-20 22:32:06 -0300

    Add método para cadastrar chamado

--- src/main/java/com/marhasoft/helpdesk/resources/ChamadoResources.java
+++ src/main/java/com/marhasoft/helpdesk/resources/ChamadoResources.java
@@ -5,11 +5,11 @@ import com.marhasoft.helpdesk.domain.dtos.ChamadoDTO;
 import com.marhasoft.helpdesk.services.ChamadoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -31,5 +31,12 @@ public class ChamadoResources {
         List<Chamado> chamados = chamadoService.findAll();
         List<ChamadoDTO> chamadosDTO = chamados.stream().map(obj -> new ChamadoDTO(obj)).collect(Collectors.toList());
         return ResponseEntity.ok().body(chamadosDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<ChamadoDTO> create(@Valid @RequestBody ChamadoDTO chamadoDTO) {
+        Chamado chamado = chamadoService.create(chamadoDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(chamado.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
--- src/main/java/com/marhasoft/helpdesk/services/ChamadoService.java
+++ src/main/java/com/marhasoft/helpdesk/services/ChamadoService.java
@@ -1,6 +1,11 @@
 package com.marhasoft.helpdesk.services;
 
 import com.marhasoft.helpdesk.domain.Chamado;
+import com.marhasoft.helpdesk.domain.Cliente;
+import com.marhasoft.helpdesk.domain.Tecnico;
+import com.marhasoft.helpdesk.domain.dtos.ChamadoDTO;
+import com.marhasoft.helpdesk.domain.enums.Prioridade;
+import com.marhasoft.helpdesk.domain.enums.Status;
 import com.marhasoft.helpdesk.repositories.ChamadoRepository;
 import com.marhasoft.helpdesk.repositories.PessoaRepository;
 import com.marhasoft.helpdesk.services.exceptions.ObjectNotFoundException;
@@ -16,14 +21,37 @@ public class ChamadoService {
     @Autowired
     private ChamadoRepository chamadoRepository;
     @Autowired
-    private PessoaRepository pessoaRepository;
+    private TecnicoService tecnicoService;
+    @Autowired
+    private ClienteService clienteService;
 
     public Chamado findById(Integer id) {
         Optional<Chamado> chamado = chamadoRepository.findById(id);
-        return chamado.orElseThrow(() -> new ObjectNotFoundException("Chamado não encontado com id: "+id));
+        return chamado.orElseThrow(() -> new ObjectNotFoundException("Chamado não encontado com id: " + id));
     }
 
     public List<Chamado> findAll() {
         return chamadoRepository.findAll();
+    }
+
+    public Chamado create(ChamadoDTO chamadoDTO) {
+        return chamadoRepository.save(newChamado(chamadoDTO));
+    }
+
+    private Chamado newChamado(ChamadoDTO chamadoDTO) {
+        Tecnico tecnico = tecnicoService.findById(chamadoDTO.getTecnico());
+        Cliente cliente = clienteService.findById(chamadoDTO.getCliente());
+
+        Chamado chamado = new Chamado();
+        if (chamadoDTO.getId() != null) {
+            chamado.setId(chamadoDTO.getId());
+        }
+        chamado.setTecnico(tecnico);
+        chamado.setCliente(cliente);
+        chamado.setPrioridade(Prioridade.toEnum(chamadoDTO.getPrioridade()));
+        chamado.setStatus(Status.toEnum(chamadoDTO.getStatus()));
+        chamado.setTitulo(chamadoDTO.getTitulo());
+        chamado.setObservacoes(chamadoDTO.getObservacoes());
+        return chamado;
     }
 }
